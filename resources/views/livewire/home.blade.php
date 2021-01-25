9f46945e5df4d8724579d286237d4587a5f58294
Johan Hanses <johanhanses@gmail.com>
2021-01-25 13:38:54 +0100

    mobile font size fix

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -3,7 +3,7 @@
         <img src="img/joag2.jpg" class="mx-auto rounded-full" alt="ces't moi">
     </div>
 
-    <h2 class="text-xl sm:text-2xl md:text-4xl mt-24 text-center tracking-tight leading-10 font-bold text-gray-900 sm:leading-none">
+    <h2 class="text-base sm:text-2xl md:text-4xl mt-24 text-center tracking-tight leading-10 font-bold text-gray-900 sm:leading-none">
         Father | Developer | Guitar player | What else?
     </h2>
 </div>
